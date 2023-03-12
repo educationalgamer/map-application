95b97e5094cc12c5b6728ca19304736c4ce7db6a
educationalgamer <94797176+educationalgamer@users.noreply.github.com>
2023-03-12 13:58:48 +0530

    added

---
+++ src/services/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+const API_KEY = '4394c2046053caf80f654a3ea9b4da8b';
+const API_URL = 'http://api.openweathermap.org/data/2.5/weather';
+ 
+export const getWeather = async (city, country) => {
+    try {
+        let response = await axios.get(`${API_URL}?q=${city},${country}&appid=${API_KEY}&units=metric`);
+        return response.data;
+    } catch (error) {
+        console.log('Error while calling the api ', error.message);
+        return error.response;
+    }
+}
